c8f99a5de0c7d65f34f2186610a267439dec5dd8
geeka1 <mariasol.causarano@gmail.com>
2019-08-14 11:41:17 -0300

    backend base

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '267950133479-3gqeon3orv5gkshju57so736dfh3u2kv.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '-xReB657l0C0j-CSs7xi3ln0';
